5e948b8b40a695ee91c4826a2ed127e9d4a5f400
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2019-08-07 11:48:14 +0530

    Cand elim done

--- 2/2-Candidate-Elim.py
+++ 2/2-Candidate-Elim.py
@@ -1,30 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import pandas as pd
-import numpy as np
-
-
-# In[104]:
-
 
 df = pd.DataFrame(pd.read_csv('Data2.csv'))
-df
-
-
-# In[105]:
-
+#df
 
 attr = list(df.columns)
 class_label = str(attr[len(attr)-1])
 class_index = len(attr)-1
-
-
-# In[106]:
-
 
 #Specific and General Hypothesis to be initialised
 
@@ -40,41 +24,48 @@ G.append(G0)
 S.append(S0)
 print(G)
 
-
-# In[107]:
-
-
 #function to check for consistency of S and G hypotheses when S gets updated, it is used to eliminate candidates in latest General hypothesis that is inconsistent with training set instance
 
 def consistent():
+    global S
+    global G
     last_G = G[-1]
     #print(last_G)
     last_S = S[-1]
     #print(last_S)
     i = 0
+    print(last_G)
     if len(last_G) > 1:
+        print(last_G)
         for g_hyp in last_G:
-            print("YOOOO G_HYP")
-            print(g_hyp)
+            #print(g_hyp)
             for i in range(len(g_hyp)):
                 if g_hyp[i]!='?' and g_hyp[i] != last_S[i]:
-                    G[-1].remove(g_hyp)
+                    #print("*************** yo check **************")
+                    #print(g_hyp)
+                    #print(" inside G[-1] ")
+                    #print(G[-1])
+                    count = 0
+                    for index_1, inner_list in enumerate(G[-1]):
+                        for c in range(len(inner_list)):
+                            if inner_list[c] == g_hyp[c]:
+                                count+=1
+                        if count == len(attr)-1:
+                            G[-1].pop(index_1)
+                        else:
+                            count = 0
+                    #print(G)
 
-
-
-# In[ ]:
-
-
+print("\nBOUNDARIES OF Specfic and General Hypothesis at each step :\n")
 for index, row in df.iterrows():
-    print(index,S[index])
-    print(index,G[index])
+    print('S',index,S[index])
+    print('G',index,G[index])
     row = list(row)
     if row[class_index] == 'P':
         #if its first training example, comparing with % attr and updating S
         if S[len(S)-1][0] == '%':
             S.append(row[:-1])
             G.append(G[-1])
-            #print(G)
 
         else:
             for i in range(len(row)-1):
@@ -82,47 +73,37 @@ for index, row in df.iterrows():
                     row[i] = '?'
             S.append(row[:-1])
             G.append(G[-1])
-            #print(G)
-            #check for consistency
+            #check for consistency with generic boundary
             consistent()
 
     elif row[class_index] == 'N':
+        row = row[:len(row)-1]
         print("For negative instance entered")
         i = 0
         tmp1 = []
-        tmp2 = temp0
-        for i in range(len(row)-1):
-            #tmp2 = temp0
+        for i in range(len(row)):
+            tmp2 = [ '?' for i in range(len(attr)-1) ]
+            #print("----------0000000000000000---------------")
+            #print(tmp2)
+            #print("--------------------------")
+            #print(row)
+            #print("--------------------------")
+            #print(S[-1][i])
             if row[i] != S[-1][i] and S[-1][i] != '?':
                 tmp2[i] = S[-1][i]
+                print(tmp2)
                 tmp1.append(tmp2)
-                tmp2 = temp0
-                break
-                #print(tmp1)
+                print(tmp1)
+                continue
             elif S[-1][i] == '?':
-                #print(tmp1)
-                print("CAME HERE ALSO")
-                tmp2 = temp0
-                break
-        #tmp1.append(tmp2)
+                #print("CAME HERE ALSO")
+                continue
         S.append(S[-1])
         G.append(tmp1)
-        #print("For negative instance")
-        #print(G)
-
-
-
-# In[ ]:
-
-'''
-#print out S and G hypothesis boundaries at each step of algo
-
-print("BOUNDARIES OF Specfic and General Hypothesis at each step :")
-for i in range(0,len(S)):
-    print("S"+str(i)+":")
-    print(S[i])
-    print("G"+str(i)+":")
-    print(G[i])
-
-'''
-# In[ ]:
+    
+print('S',index+1,':',S[index])
+print('G',index+1,':',G[index])
+#printing final boundaries
+print("\n\nFINAL Specific and Generic Boundaries\n\n")
+print('S boundary : ',S[index])
+print('G boundary : ',G[index])
